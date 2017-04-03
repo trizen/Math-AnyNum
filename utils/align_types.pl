4ad8f96f846a39b6bc8a9a5d000037036157406c
trizen <trizenx@gmail.com>
2017-04-03 18:12:05 +0300

    - Added test and documentation for constants.

--- utils/align_types.pl
+++ utils/align_types.pl
@@ -5,13 +5,13 @@ use strict;
 use warnings;
 
 # Align the return types to be on the same column.
-# usage: ./align_types.pl < BigNum.pm > BigNum-new.pm
+# usage: perl align_types.pl < old.pm > new.pm
 
-while(<>) {
+while (<>) {
     if (/^(\h++.+?)(#=> .*)/) {
         my $before = unpack('A*', $1);
         my $after = $2;
-        say $before . ' ' x (50-length($before)) . $after;
+        say $before . ' ' x (50 - length($before)) . $after;
     }
     else {
         print $_;
