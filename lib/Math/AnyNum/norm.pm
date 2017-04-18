61a3e89803e6f09c1f9ebd45d58eca3083e157a0
trizen <trizenx@gmail.com>
2017-04-18 18:45:22 +0300

    Code tidy.

--- lib/Math/AnyNum/norm.pm
+++ lib/Math/AnyNum/norm.pm
@@ -5,27 +5,27 @@ our ($ROUND, $PREC);
 
 Class::Multimethods::multimethod __norm__ => qw(Math::MPC) => sub {
     my ($x) = @_;
-            my $f = Math::MPFR::Rmpfr_init2($PREC);
-            Math::MPC::Rmpc_norm($f, $x, $ROUND);
-            $f;
+    my $f = Math::MPFR::Rmpfr_init2($PREC);
+    Math::MPC::Rmpc_norm($f, $x, $ROUND);
+    $f;
 };
 
 Class::Multimethods::multimethod __norm__ => qw(Math::MPFR) => sub {
     my ($x) = @_;
- Math::MPFR::Rmpfr_sqr($x, $x, $ROUND);
-            $x;
+    Math::MPFR::Rmpfr_sqr($x, $x, $ROUND);
+    $x;
 };
 
 Class::Multimethods::multimethod __norm__ => qw(Math::GMPz) => sub {
     my ($x) = @_;
-      Math::GMPz::Rmpz_mul($x, $x, $x);
-            $x;
+    Math::GMPz::Rmpz_mul($x, $x, $x);
+    $x;
 };
 
 Class::Multimethods::multimethod __norm__ => qw(Math::GMPq) => sub {
     my ($x) = @_;
- Math::GMPq::Rmpq_mul($x, $x, $x);
-            $x;
+    Math::GMPq::Rmpq_mul($x, $x, $x);
+    $x;
 };
 
 1;
