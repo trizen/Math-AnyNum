eed69dea159679f237b257f5eebbd2537378b8a1
trizen <614513+trizen@users.noreply.github.com>
2019-02-20 17:03:17 +0200

    Simplification in __bernfrac__()

--- lib/Math/AnyNum.pm
+++ lib/Math/AnyNum.pm
@@ -7711,10 +7711,7 @@ sub __bernfrac__ {
 
     my $log2B = (CORE::log(4 * $tau * $n) / 2 + $n * (CORE::log($n / $tau) - 1)) / CORE::log(2);
 
-    my $prec = CORE::int($n + $log2B) +
-          ($n <= 90 ? (3, 3, 4, 4, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 10, 12, 9, 7, 6, 0, 0, 0,
-                       0, 0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4)[($n>>1)-1] : 0);
-
+    my $prec = CORE::int($n + $log2B) + ($n <= 90 ? 12 : 0);
     state $d = Math::GMPz::Rmpz_init_nobless();
     Math::GMPz::Rmpz_fac_ui($d, $n);                      # d = n!
 
