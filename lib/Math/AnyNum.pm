3925ddf3a76e2c5565968c0014bb41e6b0597704
trizen <trizen@protonmail.com>
2022-06-09 22:33:06 +0300

    Support (a/b)%m when m is not coprime to b.
    Example:
    
    	say 13/3 % -12;		#=> -23/3

--- lib/Math/AnyNum.pm
+++ lib/Math/AnyNum.pm
@@ -4252,7 +4252,24 @@ sub __mod__ {
 
   Math_GMPq__Math_GMPz: {
         Math::GMPz::Rmpz_sgn($y) || goto &_nan;
-        my $r = _modular_rational($x, $y) // goto &_nan;
+        my $r = _modular_rational($x, $y) // do {
+
+            my $quo = Math::GMPq::Rmpq_init();
+            Math::GMPq::Rmpq_div_z($quo, $x, $y);
+
+            # Floor
+            Math::GMPq::Rmpq_integer_p($quo) || do {
+                my $z = Math::GMPz::Rmpz_init();
+                Math::GMPz::Rmpz_set_q($z, $quo);
+                Math::GMPz::Rmpz_sub_ui($z, $z, 1) if Math::GMPq::Rmpq_sgn($quo) < 0;
+                Math::GMPq::Rmpq_set_z($quo, $z);
+            };
+
+            Math::GMPq::Rmpq_mul_z($quo, $quo, $y);
+            Math::GMPq::Rmpq_sub($quo, $x, $quo);
+
+            return $quo;
+        };
         Math::GMPz::Rmpz_mod($r, $r, $y);
         return $r;
     }
